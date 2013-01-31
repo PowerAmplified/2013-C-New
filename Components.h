8c2e136a41b268a438500e20e259baaa19224273
Andrew Beukelman <ajbeukelman@comcast.net>
2013-01-31 16:22:12 -0600

    Added constants for components

--- Components.h
+++ Components.h
@@ -19,12 +19,23 @@ public:
 	//Init Componenets for Da Hoppa
 	Jaguar jaguarHopper;
 	
-	//Init Compressor Components
-	Relay relayComp;
-	
 	//Init Pneumatic Componenets
 	Solenoid solenoidPneu;
 	
+	//Init Compressor
+	Compressor comp;
+	
 private:
+	const int talonUpRPort = 1;
+	const int talonUpLPort = 2; 
+	const int talonDownRPort = 3;
+	const int talonDownLPort = 4;
+	const int victorUpPort = 5;
+	const int victorDownPort = 6;
+	const int jaguarHopperPort = 7;
+	const int solenoidPneuPort = 8;
+	const int compPressurePort = 1;
+	const int compRelayPort = 1;
+   
 	
 };
