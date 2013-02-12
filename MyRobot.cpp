052de611b4e42b557b9ce27a57f99b68ffd3dbb7
Alex O'Neill <oneill.alexander.b@gmail.com>
2013-02-12 16:37:47 -0600

    Removed XBox Support

--- MyRobot.cpp
+++ MyRobot.cpp
@@ -11,13 +11,17 @@ class RobotDemo : public SimpleRobot
 {
 	//Our classes.  Can only be accessed within RobotDemo.
 	Components components;
-	Joystick jXbox;
+	//Joystick jXbox;
+	Joystick lJoy;
+	Joystick rJoy;
 
 public:
 	//Define the constructor for RobotDemo
 	RobotDemo(void):
 		components(),
-		jXbox(1)
+		//jXbox(1)
+		lJoy(1),
+		rJoy(2)
 	{
 		//Set some stuff
 		components.roberto.SetExpiration(0.1);
@@ -61,11 +65,12 @@ public:
 		while (IsOperatorControl())
 		{
 			// This is actually mechanum drive, now.
-			components.roberto.MecanumDrive_Cartesian(jXbox.GetX(), jXbox.GetY(), jXbox.GetRawAxis(4));
+			//components.roberto.MecanumDrive_Cartesian(jXbox.GetX(), jXbox.GetY(), jXbox.GetRawAxis(4));
+			components.roberto.MecanumDrive_Cartesian(lJoy.GetX(), lJoy.GetY(), rJoy.GetX());
 			
 			//This part will take inputs from our gamepad and change the speed of the shooter by increments of .1 between 0 and 1.
-			components.shooter.speed = max(min(components.shooter.speed+(0.1*jXbox.GetRawButton(8))-(0.1*jXbox.GetRawButton(7)),1),0);
-			components.shooter.Set(components.shooter.speed*(components.shooter.direction*2-1));
+			//components.shooter.speed = max(min(components.shooter.speed+(0.1*jXbox.GetRawButton(8))-(0.1*jXbox.GetRawButton(7)),1),0);
+			//components.shooter.Set(components.shooter.speed*(components.shooter.direction*2-1));
 			
 			// Wait for a motor update time
 			Wait(0.005);
